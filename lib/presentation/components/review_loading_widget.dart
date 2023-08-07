45486891bedb57ed2b5a1d112deb7f855ff147c3
Muhamad Bagus <prasetyobagus27@gmail.com>
2023-08-07 22:58:39 +0700

    Remove border radius on review widget

--- lib/presentation/components/review_loading_widget.dart
+++ lib/presentation/components/review_loading_widget.dart
@@ -9,87 +9,84 @@ class ReviewLoadingWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10.0),
-      child: CustomShimmer(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(10.0),
-                  child: Container(
-                    color: Colors.black,
-                    height: 14.0,
-                    width: 100.0,
+    return CustomShimmer(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10.0),
+                child: Container(
+                  color: Colors.black,
+                  height: 14.0,
+                  width: 100.0,
+                ),
+              ),
+              const SizedBox(height: 4.0),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(10.0),
+                    child: Container(
+                      color: Colors.black,
+                      height: 14.0,
+                      width: 70.0,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 4.0),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(10.0),
-                      child: Container(
-                        color: Colors.black,
-                        height: 14.0,
-                        width: 70.0,
-                      ),
-                    ),
-                    const SizedBox(width: 4.0),
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(10.0),
-                      child: Container(
-                        color: Colors.black,
-                        height: 12.0,
-                        width: 70.0,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            const SizedBox(height: 8.0),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [null, null, null, Utils.size(context).width / 2]
-                      .map((e) => Padding(
-                            padding: const EdgeInsets.only(bottom: 4.0),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(10.0),
-                              child: Container(
-                                color: Colors.black,
-                                height: 12.0,
-                                width: e,
-                              ),
-                            ),
-                          ))
-                      .toList(),
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: ClipRRect(
+                  const SizedBox(width: 4.0),
+                  ClipRRect(
                     borderRadius: BorderRadius.circular(10.0),
                     child: Container(
                       color: Colors.black,
                       height: 12.0,
-                      width: 60,
+                      width: 70.0,
                     ),
                   ),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(height: 8.0),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [null, null, null, Utils.size(context).width / 2]
+                    .map((e) => Padding(
+                          padding: const EdgeInsets.only(bottom: 4.0),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10.0),
+                            child: Container(
+                              color: Colors.black,
+                              height: 12.0,
+                              width: e,
+                            ),
+                          ),
+                        ))
+                    .toList(),
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: Container(
+                    color: Colors.black,
+                    height: 12.0,
+                    width: 60,
+                  ),
                 ),
-              ],
-            ),
-          ],
-        ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
--- lib/presentation/components/review_widget.dart
+++ lib/presentation/components/review_widget.dart
@@ -23,62 +23,59 @@ class _ReviewWidgetState extends State<ReviewWidget> {
     final rating = widget.review.authorDetails?.rating?.toDouble() ?? 0;
     final createdAt = widget.review.createdAt;
 
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                '${widget.review.author}',
-                style: const TextStyle(fontWeight: FontWeight.w500),
-              ),
-              const SizedBox(height: 4.0),
-              Row(
-                children: [
-                  RatingBar(rating: rating, size: 14.0),
-                  const SizedBox(width: 4.0),
-                  Text(
-                    '${createdAt != null ? DateFormat('yyyy-MM-dd').format(createdAt) : null}',
-                    style: const TextStyle(fontSize: 12.0),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          const SizedBox(height: 8.0),
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                _isExpanded = !_isExpanded;
-              });
-            },
-            child: Column(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '${widget.review.author}',
+              style: const TextStyle(fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(height: 4.0),
+            Row(
               children: [
+                RatingBar(rating: rating, size: 14.0),
+                const SizedBox(width: 4.0),
                 Text(
-                  '${widget.review.content}',
-                  overflow: _isExpanded ? null : TextOverflow.ellipsis,
-                  maxLines: _isExpanded ? null : 4,
+                  '${createdAt != null ? DateFormat('yyyy-MM-dd').format(createdAt) : null}',
                   style: const TextStyle(fontSize: 12.0),
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Text(
-                    'show ${_isExpanded ? 'less' : 'more'}',
-                    style: TextStyle(
-                      color: Theme.of(context).primaryColor,
-                      fontSize: 12.0,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
                 ),
               ],
             ),
+          ],
+        ),
+        const SizedBox(height: 8.0),
+        GestureDetector(
+          onTap: () {
+            setState(() {
+              _isExpanded = !_isExpanded;
+            });
+          },
+          child: Column(
+            children: [
+              Text(
+                '${widget.review.content}',
+                overflow: _isExpanded ? null : TextOverflow.ellipsis,
+                maxLines: _isExpanded ? null : 4,
+                style: const TextStyle(fontSize: 12.0),
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Text(
+                  'show ${_isExpanded ? 'less' : 'more'}',
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontSize: 12.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
